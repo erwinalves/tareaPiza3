15d8be881124f2a68b90cc7f5d519bd154d4cb57
avasquezb <tito.vasquez7@gmail.com>
2018-08-17 11:06:45 -0300

    final

--- tarea3.cpp
+++ tarea3.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
+#include <string>
 
 using namespace std;
 
@@ -11,12 +12,12 @@ int ConvierteANumero(string valor){
 
 bool esNumero(string valor){
 	int aux = atoi(valor.c_str());
-	if(aux==0){
-		return false;
-	}
-	else{
+	//if(aux==0){
+	//	return false;
+	//}
+	//else{
 		return true;
-	}
+	//}
 }
 
 void mostrarMatriz(int matriz[][9]){
@@ -28,34 +29,85 @@ void mostrarMatriz(int matriz[][9]){
 	}
 }
 
+void extraerDatos(string argumento, string pos_i, string pos_j, string num){
+	pos_i=argumento.substr(1,1);
+	pos_j=argumento.substr(3,1);
+	num=argumento.substr(5,1);
+}
+
+void agregarNumero(){
+
+}
+
+void verificar(){
+
+}
+
 int main(int argc, char* argv[]){
 	int matriz[9][9];
 	for(int i=0;i<9;i++)
 		for(int j=0;j<9;j++)
 			matriz[i][j]=0;
-	string pos_i=argv[1];
-	string pos_j=argv[2];
-	string num=argv[3];
-	if(esNumero(pos_i) && esNumero(pos_j) && esNumero(num)){
-		if(ConvierteANumero(pos_i)>=0 && ConvierteANumero(pos_i)<9){
-			if(ConvierteANumero(pos_j)>=0 && ConvierteANumero(pos_j)<9){
-				if(ConvierteANumero(num)>=1 && ConvierteANumero(num)<=9){
-					matriz[ConvierteANumero(pos_i)][ConvierteANumero(pos_j)]=ConvierteANumero(num);
-					mostrarMatriz(matriz);
+
+	string argumento=argv[1];
+	string pos_i;
+	string pos_j;
+	string num;
+	if (argumento.substr(0,1)=="[")
+	{
+		while(argumento!="")
+		{
+			//extraerDatos(argumento, pos_i, pos_j, num);
+			pos_i=argumento.substr(1,1);
+			//cout<<pos_i<<endl;
+			pos_j=argumento.substr(3,1);
+			//cout<<pos_j<<endl;
+			num=argumento.substr(5,1);
+			//cout<<num<<endl;
+			if(esNumero(pos_i) && esNumero(pos_j) && esNumero(num))
+			{
+				//cout<<"si"<<endl;
+				if(ConvierteANumero(pos_i)>=0 && ConvierteANumero(pos_i)<9)
+				{
+					if(ConvierteANumero(pos_j)>=0 && ConvierteANumero(pos_j)<9)
+					{
+						if(ConvierteANumero(num)>=1 && ConvierteANumero(num)<=9)
+						{
+							if (matriz[ConvierteANumero(pos_i)][ConvierteANumero(pos_j)]==0)
+							{
+								matriz[ConvierteANumero(pos_i)][ConvierteANumero(pos_j)]=ConvierteANumero(num);
+							}
+							else
+							{
+								cout<<"error de formato"<<endl;
+							}
+							//mostrarMatriz(matriz);
+						}
+						else
+						{
+							cout<<"error de formato"<<endl;
+						}
+					}
+					else
+					{
+						cout<<"error de formato"<<endl;
+					}
 				}
-				else{
+				else
+				{
 					cout<<"error de formato"<<endl;
 				}
 			}
-			else{
+			else
+			{
 				cout<<"error de formato"<<endl;
 			}
+			argumento=argumento.substr(7);
 		}
-		else{
-			cout<<"error de formato"<<endl;
-		}
+		mostrarMatriz(matriz);
 	}
-	else{
+	else
+	{
 		cout<<"error de formato"<<endl;
 	}
 }
