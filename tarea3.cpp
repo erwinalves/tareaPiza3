7fbb74ac2e9649adb7e2bbbc04469812b44e9b89
erwinalves <erwinalbo@hotmail.com>
2018-08-17 14:00:44 -0300

    the real final

--- tarea3.cpp
+++ tarea3.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <cstdlib>
 #include <string>
+#include <vector> 
 
 using namespace std;
 
@@ -11,13 +12,19 @@ int ConvierteANumero(string valor){
 }
 
 bool esNumero(string valor){
-	int aux = atoi(valor.c_str());
-	//if(aux==0){
-	//	return false;
-	//}
-	//else{
+	if(ConvierteANumero(valor)==0){
 		return true;
-	//}
+	}
+	else{
+		int aux = atoi(valor.c_str());
+		if(aux==0){
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
+
 }
 
 void mostrarMatriz(int matriz[][9]){
@@ -29,85 +36,67 @@ void mostrarMatriz(int matriz[][9]){
 	}
 }
 
-void extraerDatos(string argumento, string pos_i, string pos_j, string num){
-	pos_i=argumento.substr(1,1);
-	pos_j=argumento.substr(3,1);
-	num=argumento.substr(5,1);
-}
-
-void agregarNumero(){
-
-}
-
-void verificar(){
-
-}
-
 int main(int argc, char* argv[]){
 	int matriz[9][9];
 	for(int i=0;i<9;i++)
 		for(int j=0;j<9;j++)
 			matriz[i][j]=0;
 
-	string argumento=argv[1];
+	string argumento;
 	string pos_i;
 	string pos_j;
 	string num;
-	if (argumento.substr(0,1)=="[")
-	{
-		while(argumento!="")
-		{
-			//extraerDatos(argumento, pos_i, pos_j, num);
-			pos_i=argumento.substr(1,1);
-			//cout<<pos_i<<endl;
-			pos_j=argumento.substr(3,1);
-			//cout<<pos_j<<endl;
-			num=argumento.substr(5,1);
-			//cout<<num<<endl;
-			if(esNumero(pos_i) && esNumero(pos_j) && esNumero(num))
-			{
-				//cout<<"si"<<endl;
-				if(ConvierteANumero(pos_i)>=0 && ConvierteANumero(pos_i)<9)
-				{
-					if(ConvierteANumero(pos_j)>=0 && ConvierteANumero(pos_j)<9)
-					{
-						if(ConvierteANumero(num)>=1 && ConvierteANumero(num)<=9)
-						{
-							if (matriz[ConvierteANumero(pos_i)][ConvierteANumero(pos_j)]==0)
-							{
-								matriz[ConvierteANumero(pos_i)][ConvierteANumero(pos_j)]=ConvierteANumero(num);
+	int cont=1;
+
+	for(int i=1;i<argc;i++){
+		argumento=argv[i];
+		if (argumento.substr(0,1)=="[" && argumento.substr(6,1)=="]"){
+			if(argumento.substr(2,1)=="," && argumento.substr(4,1)==","){
+				pos_i=argumento.substr(1,1);
+				pos_j=argumento.substr(3,1);
+				num=argumento.substr(5,1);
+				if(esNumero(pos_i) && esNumero(pos_j) && esNumero(num)){
+					while(argumento!=""){
+						if(ConvierteANumero(pos_i)>=0 && ConvierteANumero(pos_i)<9){
+							if(ConvierteANumero(pos_j)>=0 && ConvierteANumero(pos_j)<9){
+								if(ConvierteANumero(num)>=1 && ConvierteANumero(num)<=9){
+									if (matriz[ConvierteANumero(pos_i)][ConvierteANumero(pos_j)]==0){
+										matriz[ConvierteANumero(pos_i)][ConvierteANumero(pos_j)]=ConvierteANumero(num);
+										cont++;
+									}
+									else{
+										cout<<"ya se ingreso este parametro: "<<argumento<<endl;
+										mostrarMatriz(matriz);
+									}
+								}
+								else{
+									cout<<"error de formato"<<endl;
+								}
 							}
-							else
-							{
+							else{
 								cout<<"error de formato"<<endl;
 							}
-							//mostrarMatriz(matriz);
 						}
-						else
-						{
+						else{
 							cout<<"error de formato"<<endl;
 						}
-					}
-					else
-					{
-						cout<<"error de formato"<<endl;
+						argumento=argumento.substr(7);
 					}
 				}
-				else
-				{
+				else{
 					cout<<"error de formato"<<endl;
 				}
 			}
-			else
-			{
+			else{
 				cout<<"error de formato"<<endl;
 			}
-			argumento=argumento.substr(7);
 		}
-		mostrarMatriz(matriz);
-	}
-	else
-	{
-		cout<<"error de formato"<<endl;
+		else{
+			cout<<"error de formato"<<endl;
+		}
+		if(cont==argc){
+			mostrarMatriz(matriz);
+			cout<<endl;	
+		}
 	}
 }
